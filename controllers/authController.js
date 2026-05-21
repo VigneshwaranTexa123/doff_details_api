aba66e739f728d31ca40fc815929a95af6cad949
VIgneshwaran-UFS <vigneshultraflysolution@gmail.com>
2026-05-21 11:49:26 +0530

    vgh

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,20 +4,16 @@ export const loginUser = (req, res) => {
 
   const { user_name, password } = req.body;
 
-  console.log("Request Body:", req.body);
+  console.log("Username:", user_name);
+  console.log("Password:", password);
 
-  const sql = `
-    SELECT *
-    FROM user_details
-    WHERE user_name = ?
-    AND password = ?
-    AND status = 1
-  `;
+  // Check all users first
+  const sql = "SELECT * FROM user_details";
 
-  db.query(sql, [user_name, password], (err, result) => {
+  db.query(sql, (err, result) => {
 
     if (err) {
-      console.log("DB Error:", err);
+      console.log(err);
 
       return res.status(500).json({
         success: false,
@@ -25,23 +21,11 @@ export const loginUser = (req, res) => {
       });
     }
 
-    console.log("DB Result:", result);
+    console.log("All Users:", result);
 
-    if (result.length === 0) {
-      return res.status(401).json({
-        success: false,
-        message: "Invalid Username or Password",
-      });
-    }
-
-    return res.status(200).json({
+    return res.json({
       success: true,
-      message: "Login Successfully",
-      user: {
-        id: result[0].id,
-        user_name: result[0].user_name,
-        status: result[0].status,
-      },
+      data: result,
     });
   });
 };
